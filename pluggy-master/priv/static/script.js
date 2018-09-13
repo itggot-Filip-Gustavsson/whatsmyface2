d9bad233c1ad2d110dabe93f34a4d7ab01ec1742
kian00sh <kianoosh.geravesh@itggot.se>
2018-09-13 13:34:03 +0200

    ws-handler

--- pluggy-master/priv/static/script.js
+++ pluggy-master/priv/static/script.js
@@ -1,17 +1,34 @@
-if (top.location.pathname == "/") {
-    $(".create #create, .create #join").hide();
-    $(".create").on( "mouseenter",function() {
-        $(".create #create, .create #join").show();
-        $(".create #add").hide();
+$(document).ready(function() {
+    $("header button#account").click(function(){
+        top.location.pathname = "/login"
     });
-    $(".create").on( "mouseleave",function() {
+
+    if (top.location.pathname == "/") {
         $(".create #create, .create #join").hide();
-        $(".create #add").show();
-    });
-    $(".create #create").click(function() {
-        top.location.pathname = "/create/w"
-    });
-    $(".create #join").click(function() {
-        top.location.pathname = "/join/w"
-    });
-};
+        $(".create").on( "mouseenter",function() {
+            $(".create #create, .create #join").show();
+            $(".create #add").hide();
+        });
+        $(".create").on( "mouseleave",function() {
+            $(".create #create, .create #join").hide();
+            $(".create #add").show();
+        });
+        $(".create #create").click(function() {
+            top.location.pathname = "/create/w"
+        });
+        $(".create #join").click(function() {
+            top.location.pathname = "/join/w"
+        });
+        $(".workspaces:not(.create)").click(function(){
+            top.location.pathname = `/w/` + $(this).attr("id")
+        });
+    };
+
+    if (top.location.pathname == "/create/w") {
+        $(".ws-handler #join").hide();
+    };
+
+    if (top.location.pathname == "/join/w") {
+        $(".ws-handler #create").hide();
+    };
+});
